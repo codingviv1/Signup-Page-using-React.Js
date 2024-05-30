97c7cbc459f55551443377e50c749f170e5b43ea
codingviv1 <145456227+codingviv1@users.noreply.github.com>
2024-05-30 11:04:39 +0530

    Add files via upload

---
+++ src/components/LandingPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import BgImg from "../assets/bg.png";
+
+const LandingPage = () => {
+  return (
+    <header style={HeaderStyle}>
+      <h1 className="main-title text-center">Login Page</h1>
+      <div className="buttons text-center">
+        <Link to="/login">
+          <button className="primary-button">Log In</button>
+        </Link>
+        <Link to="/register">
+          <button className="primary-button" id="reg-btn"><span>Register</span></button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+const HeaderStyle = {
+  width: "100%",
+  height: "100vh",
+  background: `url(${BgImg})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+};
+
+export default LandingPage;
